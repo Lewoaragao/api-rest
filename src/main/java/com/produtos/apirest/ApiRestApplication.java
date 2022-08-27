9ddbc1dbbd00c301dfd4a2e2a6b802e606df1a5f
Leonardo Aragão <lewoaragao@gmail.com>
2022-08-27 09:29:14 -0300

    deploy no heroku

--- src/main/java/com/produtos/apirest/ApiRestApplication.java
+++ src/main/java/com/produtos/apirest/ApiRestApplication.java
@@ -3,17 +3,19 @@ package com.produtos.apirest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ApiRestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiRestApplication.class, args);
 	}
 	
-	@GetMapping
+	@GetMapping("/")
 	public String index() {
-		return "Olá, esse é o link para utilizar a API: https://api-rest-spring-produtos-lca.herokuapp.com/swagger-ui.html";
+		return "Olá clique aqui para usar a API: <a href='https://api-rest-spring-produtos-lca.herokuapp.com/swagger-ui.html'>link</a>.";
 	}
 
 }
