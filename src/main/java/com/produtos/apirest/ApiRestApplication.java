7408e7b6b6550309d1275474461e7dd3321400e2
Leonardo Aragão <lewoaragao@gmail.com>
2022-08-27 09:14:40 -0300

    get com link para API no heroku

--- src/main/java/com/produtos/apirest/ApiRestApplication.java
+++ src/main/java/com/produtos/apirest/ApiRestApplication.java
@@ -2,12 +2,18 @@ package com.produtos.apirest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class ApiRestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiRestApplication.class, args);
+	}
+	
+	@GetMapping
+	public String index() {
+		return "Olá, esse é o link para utilizar a API: https://api-rest-spring-produtos-lca.herokuapp.com/swagger-ui.html";
 	}
 
 }
